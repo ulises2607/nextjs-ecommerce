4e7a3de0282b62f0fd1fe8f831a5406abcba3421
Ulises <ulisescappa@gmail.com>
2024-03-25 17:23:27 -0300

    Create basic approach to the HeaderTop component. Add global styles. Modify tailwind config to manage the header top social links icons.

--- src/components/HeaderTop.jsx
+++ src/components/HeaderTop.jsx
@@ -1,8 +1,45 @@
 import React from 'react'
+import {BsFacebook, BsTwitter, BsInstagram, BsLinkedin} from "react-icons/bs"
 
 const HeaderTop = () => {
   return (
-    <div>HeaderTop</div>
+    <div className='border-b boder-gray-200 hidden sm:block'>
+      <div className='container py-4'>
+        <div className='flex justify-between item-center'>
+          <div className='hidden lg:flex gap-1'>
+            <div className='header_top__icon_wrapper'>
+            <BsFacebook />
+            </div>
+            <div className='header_top__icon_wrapper'>
+              <BsTwitter />
+            </div>
+            <div className='header_top__icon_wrapper'>
+              <BsInstagram />
+            </div>
+            <div className='header_top__icon_wrapper'>
+              <BsLinkedin />
+            </div>
+          </div>
+          <div className='text-gray-500 text-[12px]'>
+            <strong>FREE SHIPPING</strong>
+            THIS WEEK ORDER OVER - $55
+          </div>
+          <div>
+            <select name="currency" id="currency">
+              <option value="USD $">USD $</option>
+              <option value="EUR $">EUR €</option>
+              <option value="PesoArg ARS">Peso ARS</option>
+            </select>
+
+            <select name="language" id="language">
+              <option value="English">English</option>
+              <option value="Spanish">Spanish</option>
+            </select>
+          </div>
+        </div>
+        
+      </div>
+    </div>
   )
 }
 
