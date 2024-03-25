e6cca80566a8905b6dad7908b26c1286bf26afe1
Ulises <ulisescappa@gmail.com>
2024-03-25 19:40:21 -0300

    Add a navbar component  basic structure.

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <div>
+            <div>
+                <div>HOME</div>
+                <div>CATEGORIES</div>
+                <div>{`MEN'S`}</div>
+                <div>{`WOMEN'S`}</div>
+                <div>JEWELRY</div>
+                <div>PERFUME</div>
+                <div>BLOG</div>
+                <div>HOT OFFERS</div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
