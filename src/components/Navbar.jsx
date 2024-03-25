090ef652dc8b629554dc79e72cbeabc68e7f5fef
Ulises <ulisescappa@gmail.com>
2024-03-25 19:51:11 -0300

    Add navbar links styles.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,17 +2,17 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>
-        <div>
-            <div>
-                <div>HOME</div>
-                <div>CATEGORIES</div>
-                <div>{`MEN'S`}</div>
-                <div>{`WOMEN'S`}</div>
-                <div>JEWELRY</div>
-                <div>PERFUME</div>
-                <div>BLOG</div>
-                <div>HOT OFFERS</div>
+    <div className='hidden lg:block'>
+        <div className='container'>
+            <div className='flex w-fit gap-10 mx-auto font-medium py-4 text-blackish'>
+                <div className='navbar__link relative'>HOME</div>
+                <div className='navbar__link relative'>CATEGORIES</div>
+                <div className='navbar__link relative'>{`MEN'S`}</div>
+                <div className='navbar__link relative'>{`WOMEN'S`}</div>
+                <div className='navbar__link relative'>JEWELRY</div>
+                <div className='navbar__link relative'>PERFUME</div>
+                <div className='navbar__link relative'>BLOG</div>
+                <div className='navbar__link relative'>HOT OFFERS</div>
             </div>
         </div>
     </div>
