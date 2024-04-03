4166c47159b78c6d2046f82199ce3ab56d49c0b0
Ulises <ulisescappa@gmail.com>
2024-04-03 17:17:14 -0300

    Add NewProducts components and a basic const productData.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import Image from "next/image";
 import Hero from "../components/Hero";
+import NewProducts from "../components/NewProducts";
 
 export default function Home() {
   return <main>
     <Hero />
+    <NewProducts />
   </main>;
 }
