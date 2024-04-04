aa7a2f901b8271ba71fa4b403e02abc3b9250e09
Ulises <ulisescappa@gmail.com>
2024-04-04 12:14:41 -0300

    Add testimonials section with styles.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import Hero from "../components/Hero";
 import NewProducts from "../components/NewProducts";
+import Testimonials from "@/components/Testimonials";
 
 export default function Home() {
   return <main>
     <Hero />
     <NewProducts />
+    <Testimonials />
   </main>;
 }
